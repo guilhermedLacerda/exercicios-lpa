68fbe748898d8e807ae641258b82a6956f83c7bc
guilhermedLacerda <guilhermedossantoslacerda@gmail.com>
2024-03-08 10:30:28 -0300

    exercicio 7

--- routes/api.php
+++ routes/api.php
@@ -43,3 +43,9 @@ Route::get('receber/nome',function(Request $request){
                     return $resultado;
                 });
                 
+                Route::get('receber/primeiroNumero/s',function(Request $request){
+                    $primeiroNumero = $request->input('primeiroNumero');
+                    $segundoNumero = $request->input('s');
+                    $resultado = $primeiroNumero * $segundoNumero;
+                    return $resultado;
+                });
